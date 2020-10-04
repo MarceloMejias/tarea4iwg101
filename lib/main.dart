20fb762f1d4086bc6fb3897de399b99c60bfd0e0
MarceloMejias <65011000+MarceloMejias@users.noreply.github.com>
2020-10-03 23:26:33 -0300

    Tarea 4 - Introducción a la Ingeniería

---
+++ lib/main.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart'
+    show
+        AssetImage,
+        BuildContext,
+        Card,
+        CircleAvatar,
+        Colors,
+        Column,
+        EdgeInsets,
+        FontWeight,
+        Icon,
+        Icons,
+        ListTile,
+        MainAxisAlignment,
+        MaterialApp,
+        SafeArea,
+        Scaffold,
+        StatelessWidget,
+        Text,
+        TextStyle,
+        Widget,
+        runApp;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/chihau.png'),
+              ),
+              Text(
+                'Marcelo Mejías',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  fontFamily: 'Caveat',
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Estudiante de Ingeniería Civil Informática',
+                style: TextStyle(
+                  fontSize: 22.0,
+                  fontFamily: 'Caveat',
+                  color: Colors.white,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.call,
+                    color: Colors.red,
+                  ),
+                  title: Text(
+                    '+56 9 999999999',
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.red,
+                  ),
+                  title: Text(
+                    'chihau@qbo.cl',
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Text(
+                    '@',
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  title: Text(
+                    'chihau',
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.directions,
+                    color: Colors.red,
+                  ),
+                  title: Text(
+                    'Calle Falsa 123, Springfield',
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
